7cb707d3df2015fd5abc5899d6781f739f2b5a0c
ngthduy <nguyenduy170898@gmail.com>
2019-12-06 14:02:14 +0700

    3

--- mvc/views/login.php
+++ mvc/views/login.php
@@ -42,7 +42,7 @@
 				<span class="login100-form-title p-b-41">
 					KGIM
 				</span>
-				<form class="login100-form validate-form p-b-33 p-t-5" method="post" action="/KGIM/Login/UserLogin">
+				<form class="login100-form validate-form p-b-33 p-t-5" method="post" action="/project/kgim/kgim_web/Login/UserLogin">
 
 					<div class="wrap-input100 validate-input" data-validate = "Enter username">
 						<input class="input100" type="text" name="username" placeholder="User name">
